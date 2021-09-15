4f6aeb745f6656420b25369d87aed38dab704815
Usmon <usmooooon@gamil.com>
2021-09-15 18:26:19 +0500

    Project created

---
+++ app/src/main/java/io/usmon/vocabulary/MainActivity.java
@@ -0,0 +1,20 @@
+package io.usmon.vocabulary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.usmon.vocabulary.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+}
